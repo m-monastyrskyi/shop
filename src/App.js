61957f7604e5a466a8a7c3d19090db2cea6597a2
Mykola Monastyrskyi <mykola@movebo.pl>
2020-10-12 13:23:22 +0200

    added files for homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Homepage from "./Homepage";
+
+function App() {
+    return (
+        <main>
+            <Homepage />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/Homepage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./Homepage.styles.scss"
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Jackets</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Sneakers</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Shorts</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">t-shirts</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Homepage;
