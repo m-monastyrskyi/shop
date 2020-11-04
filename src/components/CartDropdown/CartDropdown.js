ac256ff1b80879f93a99213ec68c11da937931e8
Mykola Monastyrskyi <kolyan4ik@gmail.com>
2020-11-04 20:40:14 +0100

    created cartItem component and and add it to cartDropdown. Fixed cartDropdown to close on click outside itself

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomButton from '../CustomButton'
+import CartItem from '../CartItem'
 import './CartDropdown.styles.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { selectCartItems } from '../../redux/cart/cart.reducer'
 
 const CartDropdown = () => {
+    const dispatch = useDispatch()
+    const cartItems = useSelector(selectCartItems)
+
+    const handleCloseDropdown = e => {
+        if ( !document.querySelector('.cart-dropdown').contains(e.target) ) {
+            dispatch(toggleCartHidden())
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('click', handleCloseDropdown)
+        return () => window.removeEventListener('click', handleCloseDropdown)
+    }, [])
+
     return (
         <div className='cart-dropdown'>
-            <div className="cart-items"/>
+            <div className="cart-items">
+                {
+                    cartItems.map(item => <CartItem key={item.id} item={item}/>)
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
